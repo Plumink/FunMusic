8dcd7584f8a60876638968c22625f369f9141875
lvshufeng520 <1002463595@qq.com>
2020-03-31 23:40:03 +0800

    add head

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="top">
+      <Top/>
+    </div>
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+import Top from './components/head/Top';
+
+export default {
+  components: {Top}
+
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    width: 100vw;
+    height: 100vh;
+    background-color: cadetblue;
+  }
+  .top {
+    width: 100vw;
+    height: 7vh;
+    background-color: #c62f2f;
+  }
+  .left {
+    width: 19vw;
+    height: 93vh;
+    background-color: #f5f5f7;
+    float: left;
+  }
+  .right {
+    width: 81vw;
+    height: 93vh;
+    background-color: #fafafa;
+    float: left;
+  }
+
+</style>
